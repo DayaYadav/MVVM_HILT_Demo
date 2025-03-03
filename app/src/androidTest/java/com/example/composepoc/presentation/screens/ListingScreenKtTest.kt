f1c4d897f7d6b1facce39d7b6d45c7ebcb5d7317
DayaYadav <daya20oct@gmail.com>
2025-03-03 11:39:33 +0530

    Updated test class

--- app/src/androidTest/java/com/example/composepoc/presentation/screens/ListingScreenKtTest.kt
+++ app/src/androidTest/java/com/example/composepoc/presentation/screens/ListingScreenKtTest.kt
@@ -1,37 +1,69 @@
 package com.example.composepoc.presentation.screens
 
+import android.os.Build
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.captureToImage
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollTo
 import androidx.test.core.app.takeScreenshot
+import androidx.test.filters.SdkSuppress
+import com.example.composepoc.presentation.viewmodel.ProductListVewModel
 import org.junit.Before
 import org.junit.Rule
-import org.junit.jupiter.api.Assertions.*
+import org.junit.Test
 
-import org.junit.jupiter.api.Test
+/**
+     * Instrumented test for Plant list screen
+     * Date: 24-01-2025
+     */
 
-class ListingScreenKtTest {
+    @ExperimentalTestApi
+    @SdkSuppress(minSdkVersion = Build.VERSION_CODES.O)
+    class ListingScreenKtTest {
 
+  /*  private var viewModel = ProductListVewModel(
+        productListUseCase = TODO()
+    )*/
+    
+        @get:Rule
+        val composeTestRule = createComposeRule()
 
-    @get:Rule
-    val composeTestRule = createComposeRule()
-
-    @Before
-    fun setUp() {
-        // Render the Compose UI
-        composeTestRule.setContent {
-            // Call the entry point composable function
-            listingScreen()
+        @Before
+        fun plantUIComponentTest() {
+            // Render the Compose UI
+            composeTestRule.setContent {
+                // Call the entry point composable function
+                ListingScreen()
+            }
         }
 
-    }
+
+        @Test
+        fun validatelabelScreenshot() {
+
+            composeTestRule.onNodeWithText("\uD83C\uDF3F  Wardrobe Essentials").assertIsDisplayed()
+            composeTestRule.onNodeWithText("\uD83C\uDF3F  Wardrobe Essentials").captureToImage()
+            takeScreenshot()
+        }
 
 
-    @Test
-    fun listingScreen() {
-        composeTestRule.onNodeWithText("Mens Casual Slim Fit").performScrollTo()
-        composeTestRule.onNodeWithText("Mens Casual Slim Fit").performClick()
-        takeScreenshot()
+        @Test
+        fun scrollListScreenshot() {
+
+            composeTestRule.onNodeWithText("Mens Casual Slim Fit").performScrollTo()
+            takeScreenshot()
+        }
+
+
+        @Test
+        fun clickEventTest() {
+            composeTestRule.onNodeWithText("Mens Casual Slim Fit").performClick()
+            takeScreenshot()
+        }
+
+
     }
-}
+
--- app/src/main/java/com/example/composepoc/presentation/screens/ListingScreen.kt
+++ app/src/main/java/com/example/composepoc/presentation/screens/ListingScreen.kt
@@ -3,12 +3,17 @@ package com.example.composepoc.presentation.screens
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -31,6 +36,22 @@ fun ListingScreen (){
     val context  = LocalContext.current
     var result = viewModel.productList.value
 
+   /*     Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(vertical = 25.dp)
+            ,
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+
+        ) {
+            Text(
+                "Wardrobe Essentials List",
+                style = MaterialTheme.typography.headlineMedium
+            )
+        }
+
     if(result.isLoading){
         Column(modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -38,11 +59,28 @@ fun ListingScreen (){
             CircularProgressIndicator(modifier = Modifier.size(50.dp))
         }
 
-    }
+    }*/
 
     result.data?.let {
         Column(modifier = Modifier.fillMaxSize()) {
             LazyColumn {
+                item {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight()
+                            .padding(vertical = 25.dp)
+                        ,
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+
+                    ) {
+                        Text(
+                            "\uD83C\uDF3F   Wardrobe Essentials",
+                            style = MaterialTheme.typography.headlineMedium
+                        )
+                    }
+                }
                 items(it) { item ->
                     listItem(item) { product ->
                         Toast.makeText(context, product.title, Toast.LENGTH_SHORT).show()
